380c13d49dffb4cfcda7f26f43f74642d687a0ea
vicky-dot20 <svigneshkumar110@gmail.com>
2024-07-17 22:29:45 +0530

    server update

--- src/main/grpc/server.ts
+++ src/main/grpc/server.ts
@@ -2,4 +2,42 @@
 
 import * as grpc from "@grpc/grpc-js"
 import Logger from "../../frameworks/logger/winston.logger.impl"
-import { decryptPayloadRouter}
+import { decrptPayloadRouter} from "../../routes/grpc/enrichedPayload.route"
+
+const PORT = 8000;
+const logger = new Logger();
+
+const cleanup= (server: grpc.Server)=>{
+  logger.warning("Server cleanup!");
+}
+
+const getServer=()=>{
+  const server = new grpc.Server();
+  return server;
+}
+
+const main =()=>{
+  const server = getServer();
+  const host =`0.0.0.0:${PORT}`;
+  const creds = grpc.ServerCredentials.createInsecure();
+  server.addService(
+    decrptPayloadRouter.service,
+    decrptPayloadRouter.handlers,
+  );
+  decrptPayloadRouter.reflection.addToServer(server);
+  process.on("SIGINFO",()=>
+  {
+    logger.info("Caught interupr signal")
+    cleanup(server);
+  })
+  logger.info(server.getChannelzRef());
+
+  server.bindAsync(host,creds,(err,_)=>{
+    if(err){
+      logger.error(`${err.name}: ${err.message}`);
+      return cleanup(server);
+    }
+    logger.info(`your server stated on port 0.0.0.0:${PORT}`)
+  });
+};
+main();
--- src/routes/grpc/enrichedPayload.route.ts
+++ src/routes/grpc/enrichedPayload.route.ts
@@ -6,6 +6,9 @@ import { EnrichmentResponse } from "../../proto/enrichment/enrichmentPackage/Enr
 import { EnrichmentServiceHandlers } from "../../proto/enrichment/enrichmentPackage/EnrichmentService"
 import enrichPayloadController from "../../controllers/enrichPayload.controller"
 import { ReflectionService } from "@grpc/reflection"
+import { SecurePayload } from "../../domain/entities/securePayload.entity"
+import { OperationalError } from "../../domain/error/opeartional.error"
+import { ERRORS, GRPC_ERRORS } from "../../frameworks/error/constants"
 
 const packageDef = protoLoader.loadSync(VALIDATION_PROTO_FILE);
 const grpcObj = grpc.loadPackageDefinition(
@@ -13,5 +16,27 @@ const grpcObj = grpc.loadPackageDefinition(
 )as unknown as ProtoGrpcType;
 const enrichmentPackage = grpcObj.enrichmentPackage;
 const reflection = new ReflectionService(packageDef);
+export const decrptPayloadRouter ={
+  service: enrichmentPackage.EnrichmentService.service,
+  handlers:{
+    validate: async (req: { request: { payload: string } }, callBack: (arg0: null, arg1: { payload?: string; code?: grpc.status; name?: string; details?: string }) => void) => {
+      const input = req.request as SecurePayload
+      try {
+        const result = await enrichPayloadController(input).handle<EnrichmentResponse>()
+
+        callBack(null, { payload: JSON.stringify(result.payload) })
+      } catch (err) {
+        const error = (err as OperationalError).name as keyof typeof ERRORS
+        callBack(null,{
+          code: GRPC_ERRORS[error],
+          name: (err as OperationalError).name,
+          details: (err as OperationalError).tostring(),
+        })
+      }
+    },
+  } as unknown as   EnrichmentServiceHandlers,
+    reflection: reflection
+  };
+
 
 
